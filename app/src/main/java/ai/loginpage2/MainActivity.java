eca55385a60bbf4c1bb55f285afc62306f26116e
agusibrahim <pymobile123@gmail.com>
2017-07-27 16:20:31 +0700

    Initial commit

---
+++ app/src/main/java/ai/loginpage2/MainActivity.java
@@ -0,0 +1,14 @@
+package ai.loginpage2;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
